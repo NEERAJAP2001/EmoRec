37d5718ed25e9b9c4998a54821bb54a80fc24e03
NEERAJ ADITYANANTH POLAMPALLI <65017645+NEERAJAP2001@users.noreply.github.com>
2021-01-15 17:30:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,36 +5,38 @@ import pickle
 import numpy as np
 import nltk
 
-# Load the Random Forest CLassifier model
+
+# load pickle files 
 filename = 'nlp.pkl'
-classifier = pickle.load(open(filename, 'rb'))
-cv = pickle.load(open('transform.pkl', 'rb'))
+classifier = pickle.load(open(filename, 'rb'))  ## classfier model
+cv = pickle.load(open('transform.pkl', 'rb'))   ## this is used for transforming the data using CV class
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html')          ## inital home page
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    errors=[]
+    errors=[]                                     ## for finding out errors
     if request.method == 'POST':
-        try:
-            message = request.form['text']
-            data = [message]
-            vect = cv.transform(data).toarray()
-            prediction = classifier.predict(vect)
-            string = " "
-            emotion = string.join(prediction)
+        try:                                      ## try block starts here
+            message = request.form['text']        ## get the data from home.html
+            data = [message]                  
+            vect = cv.transform(data).toarray()   ## transform them to array for prediction
+            prediction = classifier.predict(vect) ## predict over the model
+            string = " "                          ## an empty string , will be used for getting the emotion  
+            emotion = string.join(prediction)     ## join to return the answer
         except:
             errors.append(
-                "Unable to get URL. Please make sure it's valid and try again."
+                "Unable to get URL. Please make sure it's valid and try again."   ## If any errors would , then this block would execute
             )
-    return render_template('home.html', prediction=emotion, errors=errors)
+    return render_template('home.html', prediction=emotion, errors=errors)        ## render the page for the user
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':     ## Execution starts here
     app.run(debug=True)
